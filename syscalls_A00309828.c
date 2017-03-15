dabc26728dc93ef0c115bdd91d64b1361d9fdb45
emmanuel8664 <emmanuel1441@hotmail.com>
2017-03-14 22:34:42 -0500

    Create syscalls_A00309828.c

---
+++ syscalls_A00309828.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdlib.h>
+
+
+
+main() {
+  
+
+ 
+printf("Tiempo del sistema con precisión de segundos %d \n",time(NULL));
+  
+  
+sleep(3);
+
+  
+
+printf("Tiempo del sistema con precisión de segundos %d \n",time(NULL));
+ 
+ 
+
+  
+  time_t current_time;
+  
+    char* c_time_string;
+  
+    current_time = time(NULL);
+  
+    c_time_string = ctime(&current_time);
+  
+ 
+printf("time() convertido a formato de tiempo local GMT 0 %s", c_time_string);
+
+
+return 0;
+
+
+}
